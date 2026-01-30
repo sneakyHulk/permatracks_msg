7db2fddb055830a90fa5e157fcf04bd3ba81bdf0
Lukas Heyn <lukas.heyn@gmail.com>
2026-01-30 09:47:14 +0100

    small common deps fix

--- include/Message.h
+++ include/Message.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#if __has_include(<common_time.h>)
 #include <common_time.h>
+#endif
 
 #include <cstdint>
 #include <ostream>
@@ -13,7 +15,10 @@ struct Message : public Type {
 
 template <typename Type>
 std::ostream& operator<<(std::ostream& os, Message<Type> const& msg) {
+#if __has_include(<common_time.h>)
 	os << "{'timestamp': " << common::from_uint64_t(msg.timestamp) << ", 'src': " << msg.src << ", 'data': " << static_cast<Type const&>(msg) << "}";
-
+#else
+	os << "{'timestamp': " << msg.timestamp << ", 'src': " << msg.src << ", 'data': " << static_cast<Type const&>(msg) << "}";
+#endif
 	return os;
 }
